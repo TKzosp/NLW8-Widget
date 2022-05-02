f2a620e892469eec03e7522b0e20bf14e51304e8
TKzosp <rafaelbadu2408@gmail.com>
2022-05-02 13:36:53 -0300

    primeira aula concluida

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{tsx,css,html}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: '#8257e6'
+        }
+      }
+    }
+  },
+  plugins: []
+};
